96e52ad4a97bbbb08e77df1b73d6af5d91c0a198
ampgn <aboucal9@gmail.com>
2024-10-07 15:06:33 +0200

    Remove bachend/env.ts in cache.

--- backend/env.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-export const environment = {
-  DATABASE_USER: 'dilma',
-  DATABASE_PASSWORD: 'iC8YOwZQbkzvmWE1',
-  DATABASE_URI:
-    'mongodb+srv://dilma:iC8YOwZQbkzvmWE1@cluster0.d4hhbv3.mongodb.net/',
-};
